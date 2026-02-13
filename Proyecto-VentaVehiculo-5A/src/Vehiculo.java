501dbc53053ac420434465afb69384b708cbe12c
unknown <axzabanuelosam@ittepic.edu.mx>
2026-02-13 10:24:16 -0700

    feat:Clase vehiculo completa

---
+++ Proyecto-VentaVehiculo-5A/src/Vehiculo.java
@@ -0,0 +1,34 @@
+
+public class Vehiculo {
+    
+    private String marca;
+    private double precio;
+    private String motor;
+    
+    
+    public Vehiculo (String marca, double precio, String motor)
+    {
+    this.marca= marca;
+    this.precio = precio;
+    this.motor = motor;
+    }
+    
+    
+    
+    //              get()
+    public String getMarca(){return this.marca;};
+    public double getPrecio(){return this.precio;};
+    public String getMotor(){return this.motor;};
+    //              set()
+    
+    
+    @Override
+    public String toString()
+    {
+        return "Marca: "+ this.marca +"\n"+
+                "Precio: "+ this.precio +"\n"+
+                "Motor:" + this.motor +
+                "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        
+    } 
+}
