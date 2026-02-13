f0cf655dd7250eb3c0b604ef4e70afc7aaa300ec
unknown <axzabanuelosam@ittepic.edu.mx>
2026-02-13 10:34:28 -0700

    feat:Clase cliente por terminar;

---
+++ Proyecto-VentaVehiculo-5A/src/Cliente.java
@@ -0,0 +1,18 @@
+
+public class Cliente {
+    
+    private String rs;
+    private String rfc;
+    private String email;
+    
+    public Cliente(String rs, String rfc, String email){
+    this.rs = rs;
+    this.rfc = rfc;
+    this.email = email;
+    }
+    
+    @Override
+    public String toString(){return "Rs: "+ this.rs+"\n"
+            + "RFC: "+ this.rfc + "\n" +
+            "Email: "+ this.email +"\n";}
+}
